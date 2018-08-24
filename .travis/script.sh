cdef6c19e4d5b17e633d77a77a9831ddd7a797d8
gjc13 <gjc@google.com>
2018-08-24 13:50:04 +0800

    [fix-nat64-ping] travis ci to test nat64

--- .travis/script.sh
+++ .travis/script.sh
@@ -73,9 +73,13 @@ scan-build)
     ;;
 
 script-check)
+    cat /proc/sys/net/ipv6/conf/all/forwarding
+    echo '++++++++++++++++++++++++++++++++++++++++++++'
+    cat /proc/sys/net/ipv6/conf/all/forwarding | grep 1 || die
+    cat /proc/sys/net/ipv6/conf/all/disable_ipv6 | grep 0 || die
     RELEASE=1 ./script/bootstrap || die 'Failed to bootstrap for release!'
     ./script/bootstrap || die 'Failed to bootstrap for development!'
-    ./script/setup || die 'Failed to setup!'
+    export NAT64=1; ./script/setup || die 'Failed to setup!'
     SOCAT_OUTPUT=/tmp/ot-socat
 
     socat -d -d pty,raw,echo=0 pty,raw,echo=0 > /dev/null 2> $SOCAT_OUTPUT &
@@ -99,10 +103,23 @@ script-check)
     sleep 10
     netstat -an | grep 49191 || die 'Service otbr-agent not ready!'
     netstat -an | grep 80 || die 'Service otbr-web not ready!'
+    cat /etc/tayga.conf
+    cat /etc/default/tayga
+    sudo service tayga start
+    sudo service otbr-nat44 start
+    sudo service tayga status
+    pidof tayga || die 'tayga not running'
+    ping6 -c 1  ::1 || die 'ipv6 failed'
+    sudo ip -6 route list
+    sudo iptables -t nat -L
+    traceroute  2001:db8:1:ffff::127.0.0.1
+    traceroute  2001:db8:1:ffff::8.8.8.8
+    ping6 -c 1  2001:db8:1:ffff::8.8.8.8 || die 'Nat64 net avaibility failed'
     kill $SERVICES_PID || die 'Failed to stop services!'
     sudo killall otbr-web || true
     sudo killall otbr-agent || true
     sudo killall wpantund || true
+    sudo service tayga stop || true
     killall ot-ncp-ftd || die 'Failed to end OpenThread!'
     killall socat || die 'Failed to end socat!'
     jobs
