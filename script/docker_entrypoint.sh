7b9c3dace443b06ab4a91a0081964e24b680561f
Jiacheng Guo <guoamtcmp@gmail.com>
2018-09-06 17:20:39 +0800

    [docker] add docker support (#183)

---
+++ script/docker_entrypoint.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+#
+#  Copyright (c) 2018, The OpenThread Authors.
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#  1. Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#  2. Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#  3. Neither the name of the copyright holder nor the
+#     names of its contributors may be used to endorse or promote products
+#     derived from this software without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+#  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+#  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+#  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+#  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+#  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+#  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+#  POSSIBILITY OF SUCH DAMAGE.
+#
+
+export LD_LIBRARY_PATH=/usr/local/lib:LD_LIBRARY_PATH
+
+function parse_args()
+{
+    while [ $# -gt 0 ]
+    do
+        echo $1
+        case $1 in
+        --ncp-path)
+            NCP_PATH=$2
+            shift
+            shift
+            ;;
+        --interface|-I)
+            TUN_INTERFACE_NAME=$2
+            shift
+            shift
+            ;;
+        --nat64-prefix)
+            NAT64_PREFIX=$2
+            shift
+            shift
+            ;;
+        --disable-default-prefix-route)
+            AUTO_PREFIX_ROUTE=false
+            shift
+            ;;
+        --disable-default-prefix-slaac)
+            AUTO_PREFIX_SLAAC=false
+            shift
+            ;;
+        *)
+            shift
+            ;;
+        esac
+    done
+}
+
+parse_args "$@"
+
+[ -n "$NCP_PATH" ] || NCP_PATH="/dev/ttyUSB0"
+[ -n "$TUN_INTERFACE_NAME" ] || TUN_INTERFACE_NAME="wpan0"
+[ -n "$AUTO_PREFIX_ROUTE" ] || AUTO_PREFIX_ROUTE=true
+[ -n "$AUTO_PREFIX_SLAAC" ] || AUTO_PREFIX_SLAAC=true
+[ -n "$NAT64_PREFIX" ] || NAT64_PREFIX="64:ff9b::/96"
+
+echo "NCP_PATH:" $NCP_PATH
+echo "TUN_INTERFACE_NAME:" $TUN_INTERFACE_NAME
+echo "NAT64_PREFIX:" $NAT64_PREFIX
+echo "AUTO_PREFIX_ROUTE:" $AUTO_PREFIX_ROUTE
+echo "AUTO_PREFIX_SLAAC:" $AUTO_PREFIX_SLAAC
+
+NAT64_PREFIX=${NAT64_PREFIX/\//\\\/}
+
+sed -i "s/^prefix.*$/prefix $NAT64_PREFIX/" /etc/tayga.conf
+sed -i "s/dns64.*{/dns64 $NAT64_PREFIX {/" /etc/bind/named.conf.options
+
+service dbus start
+service bind9 start
+service tayga start 
+wpantund \
+    -o Config:NCP:SocketPath "$NCP_PATH" \
+    -o Config:TUN:InterfaceName $TUN_INTERFACE_NAME \
+    -o Daemon:SetDefaultRouteForAutoAddedPrefix $AUTO_PREFIX_ROUTE \
+    -o IPv6:SetSLAACForAutoAddedPrefix $AUTO_PREFIX_SLAAC &
+
+sleep 5
+
+/usr/local/sbin/otbr-agent -I $TUN_INTERFACE_NAME &
+
+/usr/local/sbin/otbr-web -I $TUN_INTERFACE_NAME -p 80 &
+
+wait
