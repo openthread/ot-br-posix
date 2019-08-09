773eb2fa4a74118ce420241cc9de68b26a17f9ce
Jiacheng Guo <gjc@google.com>
2019-08-09 17:52:34 +0800

    [travis] fix travis check

--- tests/mdns/mdns_mojo_test/standalone_mojo_broker_lite.cpp
+++ tests/mdns/mdns_mojo_test/standalone_mojo_broker_lite.cpp
@@ -29,11 +29,13 @@
 #include <string>
 #include <vector>
 
-#include <base/command_line.h>
 #include "base/at_exit.h"
+#include "base/command_line.h"
 #include "base/message_loop/message_pump_for_io.h"
+#include "base/message_loop/message_pump_type.h"
 #include "base/run_loop.h"
 #include "base/task/single_thread_task_executor.h"
+#include "chromecast/external_mojo/public/cpp/common.h"
 #include "chromecast/external_mojo/public/cpp/external_mojo_broker.h"
 #include "mojo/core/embedder/embedder.h"
 #include "mojo/core/embedder/scoped_ipc_support.h"
@@ -44,7 +46,7 @@ int main(int argc, char **argv)
 {
     base::AtExitManager exit_manager;
     base::CommandLine::Init(argc, argv);
-    base::SingleThreadTaskExecutor io_task_executor(base::MessagePump::Type::IO);
+    base::SingleThreadTaskExecutor io_task_executor(base::MessagePumpType::IO);
     base::RunLoop                  run_loop;
 
     mojo::core::Configuration mojo_config;
@@ -54,7 +56,7 @@ int main(int argc, char **argv)
     mojo::core::ScopedIPCSupport ipc_support(io_task_executor.task_runner(),
                                              mojo::core::ScopedIPCSupport::ShutdownPolicy::CLEAN);
 
-    chromecast::external_mojo::ExternalMojoBroker broker;
+    chromecast::external_mojo::ExternalMojoBroker broker(chromecast::external_mojo::GetBrokerPath());
 
     run_loop.Run();
 
