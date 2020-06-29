2a6b016c00b13c98a47435cc4909a7f42c41ea55
Jonathan Hui <jonhui@google.com>
2020-06-29 14:19:51 -0700

    [scripts] apply shfmt changes (#499)

--- tests/scripts/bootstrap.sh
+++ tests/scripts/bootstrap.sh
@@ -32,9 +32,10 @@ set -euxo pipefail
 TOOLS_HOME=$HOME/.cache/tools
 [[ -d $TOOLS_HOME ]] || mkdir -p $TOOLS_HOME
 
-die() {
-	echo " *** ERROR: " $*
-	exit 1
+die()
+{
+    echo " *** ERROR: " $*
+    exit 1
 }
 
 disable_install_recommends()
@@ -51,9 +52,10 @@ APT::Get::Install-Suggests "false";
 EOF
 }
 
-install_common_dependencies() {
+install_common_dependencies()
+{
     # Common dependencies
-    sudo apt-get install --no-install-recommends -y      \
+    sudo apt-get install --no-install-recommends -y \
         libdbus-1-dev \
         ninja-build \
         doxygen \
@@ -68,7 +70,8 @@ install_common_dependencies() {
         libjsoncpp-dev
 }
 
-install_openthread_binraries() {
+install_openthread_binraries()
+{
     pip3 install -U cmake
     cd third_party/openthread/repo
     local ot_build_dir=$(VIRTUAL_TIME=0 ./script/test clean build | grep 'Build files have been written to: ' | cut -d: -f2 | tr -d ' ')
@@ -79,112 +82,113 @@ install_openthread_binraries() {
     sudo apt-get install --no-install-recommends -y socat
 }
 
-configure_network() {
+configure_network()
+{
     echo 0 | sudo tee /proc/sys/net/ipv6/conf/all/disable_ipv6
     echo 1 | sudo tee /proc/sys/net/ipv6/conf/all/forwarding
     echo 1 | sudo tee /proc/sys/net/ipv4/conf/all/forwarding
 }
 
 case "$(uname)" in
-"Linux")
-    disable_install_recommends
-    sudo apt-get update
-    install_common_dependencies
-
-    [ $BUILD_TARGET != script-check ] && [ $BUILD_TARGET != docker-check ] || {
-        install_openthread_binraries
-        configure_network
-        exit 0
-    }
-
-    [ $BUILD_TARGET != otbr-dbus-check ] || {
-        install_openthread_binraries
-        configure_network
+    "Linux")
+        disable_install_recommends
+        sudo apt-get update
         install_common_dependencies
-        exit 0
-    }
 
-    [ $BUILD_TARGET != check ] && [ $BUILD_TARGET != meshcop ] || {
-        install_openthread_binraries
-        sudo apt-get install --no-install-recommends -y avahi-daemon avahi-utils cpputest
-        configure_network
-    }
-
-    [ $BUILD_TARGET != android-check ] || {
-        sudo apt-get install --no-install-recommends -y wget unzip libexpat1-dev gcc-multilib g++-multilib
-        (
-        cd $HOME
-        wget -nv https://dl.google.com/android/repository/android-ndk-r17c-linux-x86_64.zip
-        unzip android-ndk-r17c-linux-x86_64.zip > /dev/null
-        mv android-ndk-r17c ndk-bundle
-        )
-        exit 0
-    }
-
-    [ $BUILD_TARGET != scan-build ] || {
-        pip3 install -U cmake
-        sudo apt-get install --no-install-recommends -y clang clang-tools
-    }
-
-    [ $BUILD_TARGET != pretty-check ] || {
-        sudo apt-get install -y clang-format-6.0 shellcheck
-        sudo snap install shfmt
-    }
-
-    [ "${OTBR_MDNS-}" != 'mDNSResponder' ] || {
-        SOURCE_NAME=mDNSResponder-878.30.4
-        wget https://opensource.apple.com/tarballs/mDNSResponder/$SOURCE_NAME.tar.gz &&
-        tar xvf $SOURCE_NAME.tar.gz &&
-        cd $SOURCE_NAME/mDNSPosix &&
-        make os=linux && sudo make install os=linux
-    }
-
-    # Enable IPv6
-    [ $BUILD_TARGET != check ] || (echo 0 | sudo tee /proc/sys/net/ipv6/conf/all/disable_ipv6) || die
-
-    # Allow access syslog file for unit test
-    [ $BUILD_TARGET != check ] || sudo chmod a+r /var/log/syslog || die
-
-    # Prepare Raspbian image
-    [ $BUILD_TARGET != raspbian-gcc ] || {
-        sudo apt-get install --no-install-recommends --allow-unauthenticated -y qemu qemu-user-static binfmt-support parted
-
-        (mkdir -p docker-rpi-emu \
-            && cd docker-rpi-emu \
-            && ($(git --git-dir=.git rev-parse --is-inside-work-tree) ||  git --git-dir=.git init .) \
-            && git fetch --depth 1 https://github.com/ryankurte/docker-rpi-emu.git master \
-            && git checkout FETCH_HEAD)
-
-        pip3 install git-archive-all
-
-        IMAGE_NAME=$(basename "${IMAGE_URL}" .zip)
-        IMAGE_FILE=$IMAGE_NAME.img
-        [ -f $TOOLS_HOME/images/$IMAGE_FILE ] || {
-            # unit MB
-            EXPAND_SIZE=1024
-
-            [ -d $TOOLS_HOME/images ] || mkdir -p $TOOLS_HOME/images
-
-            [[ -f $IMAGE_NAME.zip ]] || curl -LO $IMAGE_URL
-
-            unzip $IMAGE_NAME.zip -d /tmp
-
-            (cd /tmp &&
-                dd if=/dev/zero bs=1048576 count=$EXPAND_SIZE >> $IMAGE_FILE &&
-                mv $IMAGE_FILE $TOOLS_HOME/images/$IMAGE_FILE)
-
-            (cd docker-rpi-emu/scripts &&
-                sudo ./expand.sh $TOOLS_HOME/images/$IMAGE_FILE $EXPAND_SIZE)
+        [ $BUILD_TARGET != script-check ] && [ $BUILD_TARGET != docker-check ] || {
+            install_openthread_binraries
+            configure_network
+            exit 0
         }
-    }
-    ;;
 
-"Darwin")
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    ;;
+        [ $BUILD_TARGET != otbr-dbus-check ] || {
+            install_openthread_binraries
+            configure_network
+            install_common_dependencies
+            exit 0
+        }
 
-*)
-    echo "Unknown os type"
-    die
-    ;;
+        [ $BUILD_TARGET != check ] && [ $BUILD_TARGET != meshcop ] || {
+            install_openthread_binraries
+            sudo apt-get install --no-install-recommends -y avahi-daemon avahi-utils cpputest
+            configure_network
+        }
+
+        [ $BUILD_TARGET != android-check ] || {
+            sudo apt-get install --no-install-recommends -y wget unzip libexpat1-dev gcc-multilib g++-multilib
+            (
+                cd $HOME
+                wget -nv https://dl.google.com/android/repository/android-ndk-r17c-linux-x86_64.zip
+                unzip android-ndk-r17c-linux-x86_64.zip >/dev/null
+                mv android-ndk-r17c ndk-bundle
+            )
+            exit 0
+        }
+
+        [ $BUILD_TARGET != scan-build ] || {
+            pip3 install -U cmake
+            sudo apt-get install --no-install-recommends -y clang clang-tools
+        }
+
+        [ $BUILD_TARGET != pretty-check ] || {
+            sudo apt-get install -y clang-format-6.0 shellcheck
+            sudo snap install shfmt
+        }
+
+        [ "${OTBR_MDNS-}" != 'mDNSResponder' ] || {
+            SOURCE_NAME=mDNSResponder-878.30.4
+            wget https://opensource.apple.com/tarballs/mDNSResponder/$SOURCE_NAME.tar.gz \
+                && tar xvf $SOURCE_NAME.tar.gz \
+                && cd $SOURCE_NAME/mDNSPosix \
+                && make os=linux && sudo make install os=linux
+        }
+
+        # Enable IPv6
+        [ $BUILD_TARGET != check ] || (echo 0 | sudo tee /proc/sys/net/ipv6/conf/all/disable_ipv6) || die
+
+        # Allow access syslog file for unit test
+        [ $BUILD_TARGET != check ] || sudo chmod a+r /var/log/syslog || die
+
+        # Prepare Raspbian image
+        [ $BUILD_TARGET != raspbian-gcc ] || {
+            sudo apt-get install --no-install-recommends --allow-unauthenticated -y qemu qemu-user-static binfmt-support parted
+
+            (mkdir -p docker-rpi-emu \
+                && cd docker-rpi-emu \
+                && ($(git --git-dir=.git rev-parse --is-inside-work-tree) || git --git-dir=.git init .) \
+                && git fetch --depth 1 https://github.com/ryankurte/docker-rpi-emu.git master \
+                && git checkout FETCH_HEAD)
+
+            pip3 install git-archive-all
+
+            IMAGE_NAME=$(basename "${IMAGE_URL}" .zip)
+            IMAGE_FILE=$IMAGE_NAME.img
+            [ -f $TOOLS_HOME/images/$IMAGE_FILE ] || {
+                # unit MB
+                EXPAND_SIZE=1024
+
+                [ -d $TOOLS_HOME/images ] || mkdir -p $TOOLS_HOME/images
+
+                [[ -f $IMAGE_NAME.zip ]] || curl -LO $IMAGE_URL
+
+                unzip $IMAGE_NAME.zip -d /tmp
+
+                (cd /tmp \
+                    && dd if=/dev/zero bs=1048576 count=$EXPAND_SIZE >>$IMAGE_FILE \
+                    && mv $IMAGE_FILE $TOOLS_HOME/images/$IMAGE_FILE)
+
+                (cd docker-rpi-emu/scripts \
+                    && sudo ./expand.sh $TOOLS_HOME/images/$IMAGE_FILE $EXPAND_SIZE)
+            }
+        }
+        ;;
+
+    "Darwin")
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        ;;
+
+    *)
+        echo "Unknown os type"
+        die
+        ;;
 esac
