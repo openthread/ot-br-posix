586de4f1c078e127c2e089debab6e799f786a148
Jonathan Hui <jonhui@google.com>
2020-09-29 18:55:59 -0700

    [bootstrap] update brew install command (#574)
    Warning: The Ruby Homebrew installer is now deprecated and has been rewritten in
    Bash. Please migrate to the following command:
      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"

--- tests/scripts/bootstrap.sh
+++ tests/scripts/bootstrap.sh
@@ -183,7 +183,7 @@ case "$(uname)" in
         ;;
 
     "Darwin")
-        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
         ;;
 
     *)
