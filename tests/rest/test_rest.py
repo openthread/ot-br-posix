8974f9b3228a19523685bfd36f0b2eaa846af84e
Yakun Xu <xyk@google.com>
2020-11-24 15:01:50 +0800

    [rest] fix show ip address on little endian system (#619)

--- tests/rest/test_rest.py
+++ tests/rest/test_rest.py
@@ -29,12 +29,16 @@
 
 import urllib.request
 import urllib.error
+import ipaddress
 import json
 import re
 from threading import Thread
 
 rest_api_addr = "http://0.0.0.0:8081"
 
+
+def assert_is_ipv6_address(string):
+    assert (type(ipaddress.ip_address(string)) is ipaddress.IPv6Address)
 
 def get_data_from_url(url, result, index):
     response = urllib.request.urlopen(urllib.request.Request(url))
@@ -148,9 +152,7 @@ def diagnostics_check(data):
 
         for ip6_address in ip6_address_list:
             assert (type(ip6_address) == str)
-            assert (re.match(
-                r'^[a-f0-9]+:[a-f0-9]+:[a-f0-9]+:[a-f0-9]+:[a-f0-9]+:[a-f0-9]+:[a-f0-9]+:[a-f0-9]+$',
-                ip6_address) is not None)
+            assert_is_ipv6_address(ip6_address)
 
         mac_counters = diag["MACCounters"]
         assert (type(mac_counters) == dict)
@@ -200,9 +202,8 @@ def node_check(data):
         assert (key in data)
         assert (type(data[key]) == value)
 
-    assert (re.match(
-        r'^[a-f0-9]+:[a-f0-9]+:[a-f0-9]+:[a-f0-9]+:[a-f0-9]+:[a-f0-9]+:[a-f0-9]+:[a-f0-9]+$',
-        data["RlocAddress"]) is not None)
+    assert_is_ipv6_address(data["RlocAddress"])
+
     assert (re.match(r'^[A-F0-9]{16}$', data["ExtAddress"]) is not None)
     assert (re.match(r'[A-F0-9]{16}', data["ExtPanId"]) is not None)
 
@@ -224,9 +225,7 @@ def node_rloc_check(data):
 
     assert (type(data) == str)
 
-    assert (re.match(
-        r'^[a-f0-9]+:[a-f0-9]+:[a-f0-9]+:[a-f0-9]+:[a-f0-9]+:[a-f0-9]+:[a-f0-9]+:[a-f0-9]+$',
-        data) is not None)
+    assert_is_ipv6_address(data)
 
     return True
 
