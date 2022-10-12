cd05379ab45dc330629bcda7f00df404f4c76f96
Tony Zhou <zhouchen@gmail.com>
2022-10-12 16:55:17 +0800

    Refine Android.mk tab format.

--- Android.mk
+++ Android.mk
@@ -83,11 +83,8 @@ PROTO_GEN_DIR := $(OTBR_GEN_HEADER_DIR)/proto/
 PROTO_GEN_SOURCES := $(PROTO_GEN_DIR)/feature_flag.pb.cc
 
 $(PROTO_GEN_SOURCES): $(PROTOC) $(PROTO_FILE) $(PROTO_GEN_DIR)
-    mkdir -p $(PROTO_GEN_DIR)
-    $(PROTOC) \
-    --proto_path= $(LOCAL_PATH)/src/proto/ \
-    --cpp_out  ${PROTO_GEN_DIR} \
-    $(PROTO_FILE)
+	mkdir -p $(PROTO_GEN_DIR)
+	$(PROTOC) --proto_path= $(LOCAL_PATH)/src/proto/ --cpp_out ${PROTO_GEN_DIR} $(PROTO_FILE)
 
 $(LOCAL_PATH)/src/dbus/server/dbus_thread_object.cpp: $(OTBR_GEN_HEADER_DIR)/dbus/server/introspect.hpp $(PROTO_GEN_SOURCES)
 
