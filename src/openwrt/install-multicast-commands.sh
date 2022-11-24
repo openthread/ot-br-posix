27400364e35843ea73630bd439eb710c1f8bf896
Alexandru Tiniuc <a.tiniuc@cascoda.com>
2022-11-24 17:41:15 +0000

    WIP installation of new scripts
    still gotta do the config

---
+++ src/openwrt/install-multicast-commands.sh
@@ -0,0 +1,18 @@
+
+# config command
+#         option name 'Add a new multicast route'
+#         option command '/root/add-route.py'
+#         option param '1'
+
+# config command
+#         option name 'View existing multicast route'
+#         option command '/root/view-routes.py'
+
+# config command
+#         option param '1'
+#         option command '/root/remove-routes.py'
+#         option name 'Remove routes with string'
+
+# config command
+#         option name 'Restart smcroute & apply changes'
+#         option command 'smcroutectl restart'
