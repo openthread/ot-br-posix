4e35aa09e7569ca942ca0cc297aff11dbd7d4692
Alexandru Tiniuc <a.tiniuc@cascoda.com>
2022-11-25 16:28:49 +0000

    install and configure smcroute scripts

--- src/openwrt/install-multicast-commands.sh
+++ src/openwrt/install-multicast-commands.sh
@@ -1,18 +1,19 @@
+uci set luci.add_route=command
+uci set luci.add_route.name='Add a new multicast route'
+uci set luci.add_route.command='/usr/lib/add-route.py'
+uci set luci.add_route.param='1'
 
-# config command
-#         option name 'Add a new multicast route'
-#         option command '/root/add-route.py'
-#         option param '1'
+uci set luci.view_routes=command
+uci set luci.view_routes.name='View existing multicast route'
+uci set luci.view_routes.command='/usr/lib/view-routes.py'
 
-# config command
-#         option name 'View existing multicast route'
-#         option command '/root/view-routes.py'
+uci set luci.remove_routes=command
+uci set luci.remove_routes.param='1'
+uci set luci.remove_routes.command='/usr/lib/remove-routes.py'
+uci set luci.remove_routes.name='Remove routes with string'
 
-# config command
-#         option param '1'
-#         option command '/root/remove-routes.py'
-#         option name 'Remove routes with string'
+uci set luci.restart_smcroute=command
+uci set luci.restart_smcroute.name='Restart smcroute & apply changes'
+uci set luci.restart_smcroute.command='smcroutectl restart'
 
-# config command
-#         option name 'Restart smcroute & apply changes'
-#         option command 'smcroutectl restart'
+uci commit
