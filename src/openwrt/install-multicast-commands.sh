2e3ff3b7bb982b7312647f2591767ee17a651246
Alexandru Tiniuc <a.tiniuc@cascoda.com>
2022-11-25 17:25:18 +0000

    correct script paths

--- src/openwrt/install-multicast-commands.sh
+++ src/openwrt/install-multicast-commands.sh
@@ -1,15 +1,15 @@
 uci set luci.add_route=command
 uci set luci.add_route.name='Add a new multicast route'
-uci set luci.add_route.command='/usr/lib/add-route.py'
+uci set luci.add_route.command='/lib/add-route.py'
 uci set luci.add_route.param='1'
 
 uci set luci.view_routes=command
 uci set luci.view_routes.name='View existing multicast route'
-uci set luci.view_routes.command='/usr/lib/view-routes.py'
+uci set luci.view_routes.command='/lib/view-routes.py'
 
 uci set luci.remove_routes=command
 uci set luci.remove_routes.param='1'
-uci set luci.remove_routes.command='/usr/lib/remove-routes.py'
+uci set luci.remove_routes.command='/lib/remove-routes.py'
 uci set luci.remove_routes.name='Remove routes with string'
 
 uci set luci.restart_smcroute=command
