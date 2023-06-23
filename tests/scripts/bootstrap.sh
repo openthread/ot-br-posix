4c442c798272229dcbadd1f8b18c517f53532f5b
Stefan Agner <stefan@agner.ch>
2023-06-23 10:38:07 +0200

    [mdns] add Linux specific patches

--- tests/scripts/bootstrap.sh
+++ tests/scripts/bootstrap.sh
@@ -32,6 +32,8 @@ set -euxo pipefail
 TOOLS_HOME="$HOME"/.cache/tools
 [[ -d $TOOLS_HOME ]] || mkdir -p "$TOOLS_HOME"
 
+MDNSRESPONDER_PATCH_PATH=$(realpath "$(dirname "$0")"/../../third_party/mDNSResponder)
+
 disable_install_recommends()
 {
     OTBR_APT_CONF_FILE=/etc/apt/apt.conf
@@ -130,9 +132,13 @@ case "$(uname)" in
             wget https://github.com/apple-oss-distributions/mDNSResponder/archive/refs/tags/$SOURCE_NAME.tar.gz \
                 && mkdir -p $SOURCE_NAME \
                 && tar xvf $SOURCE_NAME.tar.gz -C $SOURCE_NAME --strip-components=1 \
-                && cd "$SOURCE_NAME"/mDNSShared \
-                && sed -i 's/__block mStatus err;/mStatus err;/' uds_daemon.c \
-                && cd ../mDNSPosix \
+                && cd "$SOURCE_NAME" \
+                && (
+                    for patch in "$MDNSRESPONDER_PATCH_PATH"/*.patch; do
+                        patch -p1 <"$patch"
+                    done
+                ) \
+                && cd mDNSPosix \
                 && make os=linux tls=no && sudo make install os=linux tls=no
         fi
 
