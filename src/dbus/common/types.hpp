65c53a34144b00209173e6f9fc15187dba73198a
GuoYuchao <yuchao.guo@hoorii.io>
2024-07-23 11:31:39 +0000

    align

--- src/dbus/common/types.hpp
+++ src/dbus/common/types.hpp
@@ -715,7 +715,7 @@ struct TrelInfo
     };
 
     bool               mEnabled;      ///< Whether TREL is enabled.
-    uint16_t          mNumTrelPeers; ///< The number of TREL peers.
+    uint16_t           mNumTrelPeers; ///< The number of TREL peers.
     TrelPacketCounters mTrelCounters; ///< The TREL counters.
 };
 
