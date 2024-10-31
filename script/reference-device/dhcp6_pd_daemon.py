558ca760a90eb4f558216c7992c70e18bedb36ca
Yang Song <yangsongcn@google.com>
2024-10-31 07:48:53 +0000

    Add pd daemon service: using systemd to run pd daemon service

--- script/reference-device/dhcp6_pd_daemon.py
+++ script/reference-device/dhcp6_pd_daemon.py
@@ -31,6 +31,7 @@ import logging
 import dbus
 import gi.repository.GLib as GLib
 import subprocess
+import threading
 import os
 
 from dbus.mainloop.glib import DBusGMainLoop
@@ -39,55 +40,81 @@ DBusGMainLoop(set_as_default=True)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 bus = dbus.SystemBus()
-thread_dbus_obj = None
-iface = None
-backup_dir = "/var/lib/ot-dhcpcd-backup"
+intended_dhcp6pd_state = None
+
+DHCP_CONFIG_FILE = "/etc/dhcpcd.conf"
+DHCP_CONFIG_PD_FILE = "/etc/dhcpcd.conf.pd"
+DHCP_CONFIG_ORIG_FILE = "/etc/dhcpcd.conf.orig"
+
+def restart_dhcpcd_with_pd_config():
+    global intended_dhcp6pd_state
+    if os.path.isfile(DHCP_CONFIG_PD_FILE):
+        try:
+            subprocess.run(["sudo", "cp", DHCP_CONFIG_PD_FILE, DHCP_CONFIG_FILE], check=True)
+            subprocess.run(["sudo", "systemctl", "daemon-reload"], check=True)
+            subprocess.run(["sudo", "service", "dhcpcd", "restart"], check=True)
+            logging.info("Successfully restarted dhcpcd service.")
+            intended_dhcp6pd_state = None
+        except subprocess.CalledProcessError as e:
+            logging.error(f"Error restarting dhcpcd service: {e}")
+            intended_dhcp6pd_state = None
+    else:
+        logging.error(f"{DHCP_CONFIG_PD_FILE} not found. Cannot apply configuration with prefix delegation.")
+        intended_dhcp6pd_state = None
+
+def restore_default_config():
+    if os.path.isfile(DHCP_CONFIG_ORIG_FILE):
+        try:
+            subprocess.run(["sudo", "cp", DHCP_CONFIG_ORIG_FILE, DHCP_CONFIG_FILE], check=True)
+            subprocess.run(["sudo", "systemctl", "daemon-reload"], check=True)
+            subprocess.run(["sudo", "dhcpcd", "--release"], check=True)
+            subprocess.run(["sudo", "service", "dhcpcd", "restart"], check=True)
+            logging.info("Successfully restored default dhcpcd config.")
+        except subprocess.CalledProcessError as e:
+            logging.error(f"Error restoring dhcpcd config: {e}")
+    else:
+        logging.error(f"{DHCP_CONFIG_ORIG_FILE} not found. Cannot restore default configuration.")
 
 def properties_changed_handler(interface_name, changed_properties, invalidated_properties):
+    global intended_dhcp6pd_state
     if "Dhcp6PdState" in changed_properties:
         new_state = changed_properties["Dhcp6PdState"]
         logging.info(f"Dhcp6PdState changed to: {new_state}")
-        if new_state == "running":
-            os.makedirs(backup_dir, exist_ok=True)
-            subprocess.run(["sudo", "cp", "/etc/dhcpcd.conf", f"{backup_dir}/dhcpcd.conf.orig"], check=True)
-            try:
-                subprocess.run(["sudo", "systemctl", "daemon-reload"], check=True)
-                subprocess.run(["sudo", "service", "dhcpcd", "restart"], check=True)
-                logging.info("Successfully restarted dhcpcd service.")
-            except subprocess.CalledProcessError as e:
-                logging.error(f"Error restarting dhcpcd service: {e}")
-        elif new_state == "stopped":
-            if os.path.isfile(f"{backup_dir}/dhcpcd.conf.orig"):
-                try:
-                    subprocess.run(["sudo", "cp", f"{backup_dir}/dhcpcd.conf.orig", "/etc/dhcpcd.conf"], check=True)
-                    logging.info("Successfully restored default dhcpcd config.")
-                except subprocess.CalledProcessError as e:
-                    logging.error(f"Error restoring dhcpcd config: {e}")
-            else:
-                logging.error("Backup configuration file not found. Cannot restore default.")
+        if new_state == "running" and intended_dhcp6pd_state != "running":
+            intended_dhcp6pd_state = "running"
+            thread = threading.Thread(target=restart_dhcpcd_with_pd_config)
+            thread.start()
+        elif new_state in ("stopped", "disabled") and intended_dhcp6pd_state is None:
+            restore_default_config()
 
 def connect_to_signal():
-    global thread_dbus_obj, thread_properties_dbus_iface
     try:
-        thread_dbus_obj = bus.get_object('io.openthread.BorderRouter.wpan0', '/io/openthread/BorderRouter/wpan0')
-        thread_properties_dbus_iface = dbus.Interface(thread_dbus_obj, 'org.freedesktop.DBus.Properties')
-        thread_dbus_obj.connect_to_signal("PropertiesChanged", properties_changed_handler, dbus_interface=thread_properties_dbus_iface.dbus_interface)
+        dbus_obj = bus.get_object('io.openthread.BorderRouter.wpan0', '/io/openthread/BorderRouter/wpan0')
+        properties_dbus_iface = dbus.Interface(dbus_obj, 'org.freedesktop.DBus.Properties')
+        dbus_obj.connect_to_signal("PropertiesChanged", properties_changed_handler, dbus_interface=properties_dbus_iface.dbus_interface)
         logging.info("Connected to D-Bus signal.")
+        return dbus_obj, properties_dbus_iface
     except dbus.DBusException as e:
         logging.error(f"Error connecting to D-Bus: {e}")
-        thread_dbus_obj = None
-        thread_properties_dbus_iface = None
+        return None, None
 
-def check_and_reconnect():
-    if thread_dbus_obj is None:
-        connect_to_signal()
+def check_and_reconnect(dbus_obj, properties_dbus_iface):
+    if dbus_obj is None:
+        dbus_obj, properties_dbus_iface = connect_to_signal()
 
 def main():
+    # always restart dhcpcd to begin with a previous saved state
+    try:
+        subprocess.run(["sudo", "systemctl", "reload-or-restart", "dhcpcd"], check=True)
+        logging.info("Successfully restarting dhcpcd service.")
+    except subprocess.CalledProcessError as e:
+        logging.error(f"Error restarting dhcpcd service: {e}")
+
     loop = GLib.MainLoop()
 
-    connect_to_signal()
+    thread_dbus_obj, thread_properties_dbus_iface = connect_to_signal()
 
-    GLib.timeout_add_seconds(5, check_and_reconnect)
+    GLib.timeout_add_seconds(5, check_and_reconnect, thread_dbus_obj, thread_properties_dbus_iface) 
 
     loop.run()
 
