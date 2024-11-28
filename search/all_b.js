75c64bb34555ff3fd4941dc65dcddbb82ee92df8
jwhui <jwhui@users.noreply.github.com>
2024-11-28 17:04:11 +0000

    deploy: db68d5bfef73adc28b04945f51126b50990d7b52

--- search/all_b.js
+++ search/all_b.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['leaderdata_354',['LeaderData',['../dd/d1d/structotbr_1_1DBus_1_1LeaderData.html',1,'otbr::DBus']]],
   ['leaderdata2jsonstring_355',['LeaderData2JsonString',['../d0/d6c/namespaceotbr_1_1rest_1_1Json.html#a88e97b6320737839ae1eb73895ff7550',1,'otbr::rest::Json']]],
-  ['leave_356',['Leave',['../d2/d1c/classotbr_1_1Ncp_1_1NcpHost.html#a4d952014815c0e074a493a1859f3bb6d',1,'otbr::Ncp::NcpHost::Leave()'],['../dd/d09/classotbr_1_1Ncp_1_1RcpHost.html#a1c9c613f851efd44feafad2a0a71239a',1,'otbr::Ncp::RcpHost::Leave()'],['../db/d04/classotbr_1_1Ncp_1_1ThreadHost.html#ab7c9fbb1889a7b1f31ddea0981ccbaef',1,'otbr::Ncp::ThreadHost::Leave()']]],
+  ['leave_356',['Leave',['../d2/d1c/classotbr_1_1Ncp_1_1NcpHost.html#aa1b3bccfd185de7f268975785c1c8bd7',1,'otbr::Ncp::NcpHost::Leave()'],['../dd/d09/classotbr_1_1Ncp_1_1RcpHost.html#a47f51da98a25a384eebc6d614e40a550',1,'otbr::Ncp::RcpHost::Leave()'],['../db/d04/classotbr_1_1Ncp_1_1ThreadHost.html#a7eba48a10b411f6d712453fc301c9322',1,'otbr::Ncp::ThreadHost::Leave()']]],
   ['linkmodeconfig_357',['LinkModeConfig',['../d1/d82/structotbr_1_1DBus_1_1LinkModeConfig.html',1,'otbr::DBus']]],
   ['logging_2ehpp_358',['logging.hpp',['../d0/da9/logging_8hpp.html',1,'']]],
   ['logopenthreadresult_359',['LogOpenThreadResult',['../d4/d44/classotbr_1_1agent_1_1ThreadHelper.html#a0569c5bec5dd73e5ffa2c99232f5a7c4',1,'otbr::agent::ThreadHelper']]],
--- search/functions_a.js
+++ search/functions_a.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['leaderdata2jsonstring_1424',['LeaderData2JsonString',['../d0/d6c/namespaceotbr_1_1rest_1_1Json.html#a88e97b6320737839ae1eb73895ff7550',1,'otbr::rest::Json']]],
-  ['leave_1425',['Leave',['../d2/d1c/classotbr_1_1Ncp_1_1NcpHost.html#a4d952014815c0e074a493a1859f3bb6d',1,'otbr::Ncp::NcpHost::Leave()'],['../dd/d09/classotbr_1_1Ncp_1_1RcpHost.html#a1c9c613f851efd44feafad2a0a71239a',1,'otbr::Ncp::RcpHost::Leave()'],['../db/d04/classotbr_1_1Ncp_1_1ThreadHost.html#ab7c9fbb1889a7b1f31ddea0981ccbaef',1,'otbr::Ncp::ThreadHost::Leave()']]],
+  ['leave_1425',['Leave',['../d2/d1c/classotbr_1_1Ncp_1_1NcpHost.html#aa1b3bccfd185de7f268975785c1c8bd7',1,'otbr::Ncp::NcpHost::Leave()'],['../dd/d09/classotbr_1_1Ncp_1_1RcpHost.html#a47f51da98a25a384eebc6d614e40a550',1,'otbr::Ncp::RcpHost::Leave()'],['../db/d04/classotbr_1_1Ncp_1_1ThreadHost.html#a7eba48a10b411f6d712453fc301c9322',1,'otbr::Ncp::ThreadHost::Leave()']]],
   ['logopenthreadresult_1426',['LogOpenThreadResult',['../d4/d44/classotbr_1_1agent_1_1ThreadHelper.html#a0569c5bec5dd73e5ffa2c99232f5a7c4',1,'otbr::agent::ThreadHelper']]],
   ['long2hex_1427',['Long2Hex',['../d4/dfd/hex_8cpp.html#a15cd9a1b807a906b5b35afeeee15c00b',1,'otbr::Utils']]]
 ];
