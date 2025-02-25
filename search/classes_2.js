a752d7e0ca6531f6ed3adbc11825dcd182203309
jwhui <jwhui@users.noreply.github.com>
2025-02-25 22:22:08 +0000

    deploy: 1a16957753e96e71fa027d976dee1a4bf0340355

--- search/classes_2.js
+++ search/classes_2.js
@@ -3,7 +3,8 @@ var searchData=
   ['channelmaxpower_0',['ChannelMaxPower',['../d3/dd3/structotbr_1_1Host_1_1ThreadHost_1_1ChannelMaxPower.html',1,'otbr::Host::ThreadHost']]],
   ['channelquality_1',['ChannelQuality',['../dd/d1a/structotbr_1_1DBus_1_1ChannelQuality.html',1,'otbr::DBus']]],
   ['childinfo_2',['ChildInfo',['../db/d06/structotbr_1_1DBus_1_1ChildInfo.html',1,'otbr::DBus']]],
-  ['comparator_3',['Comparator',['../dd/da7/structotbr_1_1TaskRunner_1_1DelayedTask_1_1Comparator.html',1,'otbr::TaskRunner::DelayedTask']]],
-  ['connection_4',['Connection',['../d4/db3/classotbr_1_1rest_1_1Connection.html',1,'otbr::rest']]],
-  ['crc16_5',['Crc16',['../d6/d8f/classotbr_1_1Crc16.html',1,'otbr']]]
+  ['clidaemon_3',['CliDaemon',['../da/de6/classotbr_1_1CliDaemon.html',1,'otbr']]],
+  ['comparator_4',['Comparator',['../dd/da7/structotbr_1_1TaskRunner_1_1DelayedTask_1_1Comparator.html',1,'otbr::TaskRunner::DelayedTask']]],
+  ['connection_5',['Connection',['../d4/db3/classotbr_1_1rest_1_1Connection.html',1,'otbr::rest']]],
+  ['crc16_6',['Crc16',['../d6/d8f/classotbr_1_1Crc16.html',1,'otbr']]]
 ];
--- search/files_2.js
+++ search/files_2.js
@@ -1,10 +1,11 @@
 var searchData=
 [
-  ['client_5ferror_2ehpp_0',['client_error.hpp',['../db/d12/client__error_8hpp.html',1,'']]],
-  ['code_5futils_2ehpp_1',['code_utils.hpp',['../da/d7b/code__utils_8hpp.html',1,'']]],
-  ['config_2eh_2',['config.h',['../db/d16/config_8h.html',1,'']]],
-  ['connection_2ehpp_3',['connection.hpp',['../dd/d42/connection_8hpp.html',1,'']]],
-  ['constants_2ehpp_4',['constants.hpp',['../d6/dc0/backbone__router_2constants_8hpp.html',1,'(Global Namespace)'],['../d4/def/dbus_2common_2constants_8hpp.html',1,'(Global Namespace)']]],
-  ['crc16_2ecpp_5',['crc16.cpp',['../d7/dd9/crc16_8cpp.html',1,'']]],
-  ['crc16_2ehpp_6',['crc16.hpp',['../d4/d31/crc16_8hpp.html',1,'']]]
+  ['cli_5fdaemon_2ehpp_0',['cli_daemon.hpp',['../d0/d8e/cli__daemon_8hpp.html',1,'']]],
+  ['client_5ferror_2ehpp_1',['client_error.hpp',['../db/d12/client__error_8hpp.html',1,'']]],
+  ['code_5futils_2ehpp_2',['code_utils.hpp',['../da/d7b/code__utils_8hpp.html',1,'']]],
+  ['config_2eh_3',['config.h',['../db/d16/config_8h.html',1,'']]],
+  ['connection_2ehpp_4',['connection.hpp',['../dd/d42/connection_8hpp.html',1,'']]],
+  ['constants_2ehpp_5',['constants.hpp',['../d6/dc0/backbone__router_2constants_8hpp.html',1,'(Global Namespace)'],['../d4/def/dbus_2common_2constants_8hpp.html',1,'(Global Namespace)']]],
+  ['crc16_2ecpp_6',['crc16.cpp',['../d7/dd9/crc16_8cpp.html',1,'']]],
+  ['crc16_2ehpp_7',['crc16.hpp',['../d4/d31/crc16_8hpp.html',1,'']]]
 ];
