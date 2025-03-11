c25a656b8db8ef1b83b126ccedff94a37a6042a1
Li Cao <irvingcl@google.com>
2025-03-11 08:21:31 +0800

    [border agent] refactor border agent to use MeshCoP Service state changed callback (#2727)
    (cherry picked from commit 861d7225ef8b5d06c992f8e272f08e77c0675d4a)

--- src/host/ncp_host.cpp
+++ src/host/ncp_host.cpp
@@ -253,6 +253,11 @@ void NcpHost::AddThreadEnabledStateChangedCallback(ThreadEnabledStateCallback aC
     OT_UNUSED_VARIABLE(aCallback);
 }
 
+void NcpHost::SetBorderAgentMeshCoPServiceChangedCallback(BorderAgentMeshCoPServiceChangedCallback aCallback)
+{
+    OTBR_UNUSED_VARIABLE(aCallback);
+}
+
 void NcpHost::Process(const MainloopContext &aMainloop)
 {
     mSpinelDriver.Process(&aMainloop);
