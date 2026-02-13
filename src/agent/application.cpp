openthread/ot-br-posix

commit 2eb9a08475172b6b1554aee9654110f42c48e8ad

[dbus] unregister the dbus object from the dbus library (#3170)
When running the `factoryreset` command on RaspberryPi, the system
generates the following error message: `otbr[2862]: [EMERG]-DBUS----:
FAILED src/dbus/server/dbus_agent.cpp:85 - Failed to initialize DBus
Agent`

This root cause is that the dbus object was not unregistered from the
dbus library when running the `factoryreset` command.  When otbr
restarts, it will register the dbus object again. Andt he dbus library
generates the error message
`org.freedesktop.DBus.Error.ObjectPathInUse`.

This commit add `Deinit()` method to unregister the dbus object from
the dbus library.

src/agent/application.cpp
@@ -123,6 +123,10 @@ void Application::Init(const std::string &aRestListenAddress, int aRestListenPor
 
 void Application::Deinit(void)
 {
+#if OTBR_ENABLE_DBUS_SERVER
+    mDBusAgent.Deinit();
+#endif
+
     switch (mHost.GetCoprocessorType())
     {
     case OT_COPROCESSOR_RCP:

src/dbus/server/dbus_agent.cpp
@@ -85,6 +85,15 @@ void DBusAgent::Init(void)
     VerifyOrDie(error == OTBR_ERROR_NONE, "Failed to initialize DBus Agent");
 }
 
+void DBusAgent::Deinit(void)
+{
+    if (mThreadObject != nullptr)
+    {
+        mThreadObject->Deinit();
+        mThreadObject = nullptr;
+    }
+}
+
 DBusAgent::UniqueDBusConnection DBusAgent::PrepareDBusConnection(void)
 {
     DBusError            dbusError;

src/dbus/server/dbus_agent.hpp
@@ -68,6 +68,8 @@ public:
      */
     void Init(void);
 
+    void Deinit(void);
+
     void Update(MainloopContext &aMainloop) override;
     void Process(const MainloopContext &aMainloop) override;
 

src/dbus/server/dbus_object.cpp
@@ -82,7 +82,28 @@ otbrError DBusObject::Initialize(bool aIsAsyncPropertyHandler)
     }
 
 exit:
+    if (error == OTBR_ERROR_DBUS)
+    {
+        otbrLogCrit("Failed to register d-bus object path: %s", mObjectPath.c_str());
+    }
+
     return error;
+}
+
+void DBusObject::Deinit(void)
+{
+    if ((mConnection != nullptr) && !mMethodHandlers.empty())
+    {
+        if (!dbus_connection_unregister_object_path(mConnection, mObjectPath.c_str()))
+        {
+            otbrLogWarning("Failed to unregister d-bus object path: %s", mObjectPath.c_str());
+        }
+    }
+
+    mMethodHandlers.clear();
+    mGetPropertyHandlers.clear();
+    mSetPropertyHandlers.clear();
+    mAsyncGetPropertyHandlers.clear();
 }
 
 void DBusObject::RegisterMethod(const std::string       &aInterfaceName,

src/dbus/server/dbus_object.hpp
@@ -100,6 +100,13 @@ public:
     virtual otbrError Init(void);
 
     /**
+     * This method de-initializes the d-bus object.
+     *
+     * This method will unregister the object from the d-bus library.
+     */
+    virtual void Deinit(void);
+
+    /**
      * This method registers the method handler.
      *
      * @param[in] aInterfaceName  The interface name.

commit df4eeee1415c1dcaf0170c86d8e001c03967dca1

[dbus] fix crash during shutdown due to early deinitialization
This commit addresses a regression introduced in #3170 where the DBus
agent was deinitialized before the OpenThread `RcpHost`, leading to a
possible crash during teardown.

When `Application::Deinit()` is called, the following sequence
can occur:

1. `mHost.Deinit()` triggers `ot::Posix::InfraNetif::TearDown()`.
2. This disables Border Routing, causing the `RoutingManager` to update
   the DHCPv6 PD state.
3. The state change triggers `ThreadHelper::BorderRoutingDhcp6PdCallback`.
4. The callback attempts to notify DBus via `DBusThreadObjectRcp`.

If the DBus agent is already deinitialized, this notification causes a
crash. This change ensures `mDBusAgent` is deinitialized last to
maintain valid references during the teardown sequence.

src/agent/application.cpp
@@ -123,10 +123,6 @@ void Application::Init(const std::string &aRestListenAddress, int aRestListenPor
 
 void Application::Deinit(void)
 {
-#if OTBR_ENABLE_DBUS_SERVER
-    mDBusAgent.Deinit();
-#endif
-
     switch (mHost.GetCoprocessorType())
     {
     case OT_COPROCESSOR_RCP:
@@ -141,6 +137,10 @@ void Application::Deinit(void)
     }
 
     mHost.Deinit();
+
+#if OTBR_ENABLE_DBUS_SERVER
+    mDBusAgent.Deinit();
+#endif
 }
 
 otbrError Application::Run(void)